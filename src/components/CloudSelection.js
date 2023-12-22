70f7220c981f339997ae9e11e24f85b3c0e4e281
codeshari <naanhariharans@gmail.com>
2023-12-22 12:30:19 +0530

    added front-page and more styles

--- src/components/CloudSelection.js
+++ src/components/CloudSelection.js
@@ -3,6 +3,12 @@
 import React from 'react';
 import './CloudSelection.css';
 import {Link} from 'react-router-dom';
+import awslogo from '../images/aws.png';
+import azurelogo from '../images/azure.png';
+import gcplogo from '../images/gcp.png';
+import oraclelogo from '../images/oracle.png';
+import terraformlogo from '../images/terraform.png';
+import kuberneteslogo from '../images/kubernetes.png';
 
 const CloudSelection = () => {
   return (
@@ -10,21 +16,21 @@ const CloudSelection = () => {
             <div className="cloud-selection">
         <Link to="/awshome" className="cloud-option aws" style={{ textDecoration: 'none' }}>
 
-          <div className="aws"><img src="https://logolook.net/wp-content/uploads/2023/09/Amazon-Web-Services-Logo.png"/></div>
+          <div className="aws"><img src={awslogo} alt="AWS Logo"/></div>
           <div className="aws-label">AWS</div>
 
         </Link>
         <Link to="/gcphome" className="cloud-option gcp" style={{ textDecoration: 'none' }}>
-          <div className="gcp"><img src="https://static-00.iconduck.com/assets.00/google-cloud-icon-2048x1646-7admxejz.png"/></div>
+          <div className="gcp"><img src={gcplogo} alt = "GCP Logo"/></div>
           <div className="gcp-label">GCP</div>
 
         </Link>
         <Link to="/azurehome" className="cloud-option azure" style={{ textDecoration: 'none' }}>
-          <div className="azure"><img src="https://swimburger.net/media/ppnn3pcl/azure.png"/></div>
+          <div className="azure"><img src={azurelogo} alt="Azure Logo"/></div>
           <div className="azure-label">Azure</div>
         </Link>
         <Link to="/oraclehome" className="cloud-option oracle" style={{ textDecoration: 'none' }}>
-          <div className="oracle"><img src="https://scioinfotech.com/wp-content/uploads/2019/10/oracle-cloud-logo-e1572318472568.png"/></div>
+          <div className="oracle"><img src={oraclelogo} alt="Oracle Cloud Logo"/></div>
           <div className="oracle-label">Oracle Cloud</div>
         </Link>
       </div>
@@ -32,12 +38,12 @@ const CloudSelection = () => {
             <div className="cloud-selection">
         <Link to="/teraformhome" className="cloud-option terraform" style={{ textDecoration: 'none' }}>
 
-          <div className="terraform"><img src="https://static-00.iconduck.com/assets.00/terraform-icon-452x512-ildgg5fd.png"/></div>
+          <div className="terraform"><img src={terraformlogo} alt="Terraform Logo"/></div>
           <div className="terraform-label">Terraform</div>
 
         </Link>
         <Link to="/kuberneteshome" className="cloud-option kubernetes" style={{ textDecoration: 'none' }}>
-          <div className="kubernetes"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/39/Kubernetes_logo_without_workmark.svg/1200px-Kubernetes_logo_without_workmark.svg.png"/></div>
+          <div className="kubernetes"><img src={kuberneteslogo} alt="Kubernetes Logo"/></div>
           <div className="kubernetes-label">Kubernetes</div>
 
         </Link>
